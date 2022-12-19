bebc85cba8897529a80601b809924ad4ed9121b1
akabir <89921745+akabir-innovaj@users.noreply.github.com>
2022-12-19 10:37:27 +0000

    feat: ensure login will not load spinner and error on start

--- src/presentation/components/form-status/FormStatus.tsx
+++ src/presentation/components/form-status/FormStatus.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Spinner from '../spinner/Spinner'
 import Styles from './form-status-styles.scss'
+import Context from '../../contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-<div className={Styles.errorWrap}>
-        <span className={Styles.error}>Error</span>
-        <Spinner className={Styles.spinner} />
+<div data-testid="error-wrap" className={Styles.errorWrap}>
+        {isLoading && <Spinner className={Styles.spinner} />}
+        {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
       </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import { LoginHeader, Footer, Input, FormStatus } from '../../components'
+import Context from '../../contexts/form/form-context'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 const login: React.FC = () => {
+  const [state] = useState<StateProps>({ isLoading: false, errorMessage: '' })
   return (
   <div className={Styles.login}>
     <LoginHeader />
+    <Context.Provider value={state}>
     <form className={Styles.form}>
       <h2>Login</h2>
         <Input type={'email'} name="email" placeholder='Enter your email' />
@@ -14,6 +22,7 @@ const login: React.FC = () => {
       <span className={Styles.link}>Create Account</span>
       <FormStatus />
     </form>
+    </Context.Provider>
     <Footer />
   </div>)
 }
