2d2a2a0ea359f913e51f90c72acc6ffa204f38e4
akabir <89921745+akabir-innovaj@users.noreply.github.com>
2022-12-19 12:24:46 +0000

    feat: ensure login calls validation eith correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -43,12 +43,20 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Required')
     expect(passwordStatus.textContent).toBe('*')
   })
-  test('Should call validation with correct value', () => {
+  test('Should call validation with correct email', () => {
     const { sut, validationSpy } = makeSut()
     const emailInput = sut.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: 'any_email' } })
     expect(validationSpy.input).toEqual({
       email: 'any_email'
+    })
+  })
+  test('Should call validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
     })
   })
 })
