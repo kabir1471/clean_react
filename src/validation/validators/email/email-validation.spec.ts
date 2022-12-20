df7d4afcd1d2d201866257dac7ed984033422cf9
akabir <89921745+akabir-innovaj@users.noreply.github.com>
2022-12-20 09:52:24 +0000

    feat: ensure emailvalidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,16 @@
+import { faker } from '@faker-js/faker'
 import { InvalidFieldError } from '../../../validation/errors'
 import { EmailValidation } from './email-validation'
 
 describe('Email Validation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+  test('Should return falsy if email is invalid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
